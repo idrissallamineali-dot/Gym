db3ed8980470433e63c7890d6304d26d6b4e4bc1
idriss allamine <thelazystudent4@gmail.com>
2026-06-16 01:20:59 +0200

    day4

---
+++ day4.js
@@ -0,0 +1,20 @@
+//Return only numbers greater than 5.
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const num = nums.filter (n => n>5);
+//Return a new array where each number is tripled.
+const num = nums.map (n => n*3);
+//Return only words that have more than 5 characters.
+const num = words.filter(n => n.length>5);
+//Return the product of all numbers multiplied together.
+const num = nums.reduce((acc,current) => acc*current,1);
+//Return only users who are 18 or older.
+const num = users.filter((n) => n.age>=18);
+//Return an array of just the names of users who are 18 or older.
+const num = users.filter( (n) => {if(n.age>18){ return n.age}});
+//Return the sum of only the even numbers.
+const num = (nums.filter( (n) => n%2==0)).reduce( (acc,current)=> acc+current,0);
+//Return a new array where every sentence is capitalized.
+const sentences = ["hello world", "js is great", "i love coding"];
+//Return the smallest number using reduce. No Math.min allowed.
+const num = nums.reduce( (acc,current) => {if(acc>current) {return current} return acc })
+//Return the sum of squares of only the odd numbers.
